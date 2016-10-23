d247f172aa383d0ff9270d36db74d46226f460ef
Deron.Gray <deron.gray@pcc.edu>
2016-10-23 13:08:00 -0700

    added main method that prints hello classmates

--- src/edu/pcc/cis/cis233jproject/CIS233JProject.java
+++ src/edu/pcc/cis/cis233jproject/CIS233JProject.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.cis233jproject;
  * Created by Deron on 10/23/2016.
  */
 public class CIS233JProject {
+    public static void main(String[] args) {
+        System.out.println("Hello Classmates");
+    }
 }
